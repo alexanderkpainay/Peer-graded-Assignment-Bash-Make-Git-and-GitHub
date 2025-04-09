311a3d2c4808c3daacc84f4def857c9a7e609d40
alexanderkpainay <167440660+alexanderkpainay@users.noreply.github.com>
2025-04-09 11:28:44 +0000

    commit

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,13 +2,29 @@
 # File: guessinggame.sh
 echo Guess the number of files in the current working directory
 read response
-fileCount=$(ls ./ | wc -l)
-if [[ $fileCount -eq $response ]]
-then 
-    echo Congratulations! You guessed correctly!
-elif [[ $response -lt $fileCount ]]
-then 
-    echo Sorry, your guess is less then the file count
-else 
-    echo Sorry, your guess is more then the file count
-fi
+
+
+function guess {
+    fileCount=$(ls ./ | wc -l)
+    while [[ 1 ]]
+    do 
+    if [[ $response -eq $fileCount ]]
+    then 
+        echo Congratulations!! You guessed correctly!
+        exit
+        
+    elif [[ $response -gt $filecount ]]
+    then 
+        echo Your guess is greater than the file count. Please try again
+        read response
+    else
+        echo Your guess is less than the file count. Please try again
+        read response
+    fi
+    done 
+}
+
+guess 
+
+
+
--- makefile
+++ makefile
@@ -1,8 +1,9 @@
 readme: guessinggame.sh
 	echo "# Peer-graded Assignment: Bash, Make, Git, and GitHub" > README.md
-	echo "Make was last run at:" >> README.md
+	echo "### Make was last run at:" >> README.md
 	date >> README.md
-	echo "The guessing game contains:" >> README.md
+	echo >> README.md
+	echo -n "### The guessing game contains: " >> README.md
 	wc -l guessinggame.sh | egrep -o [0-9]+ >> README.md
-	echo lines >> README.md
+	echo "### lines" >> README.md
 
